584a7b6dbd006a324130f8fd47b9413402645361
rerecum <kacperkacperkacper132@gmail.com>
2021-12-31 00:21:54 +0100

    dodanie funkcji merge

--- Merge Sorting/Merge Sorting.cpp
+++ Merge Sorting/Merge Sorting.cpp
@@ -2,8 +2,28 @@
 
 using namespace std;
 
+
+
 int main()
 {
+    //int arr[5] = { 4,6,1,2,7 };
+    int arr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Przed sortowaniem Merge: " << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
 
+    // mergesort function
+
+    cout << "Po sortowaniu Merge: " << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
